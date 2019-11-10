06b27f9db1fc0662117531c71997b288014df9d1
Michael Eisel <michael@weav.io>
2019-11-10 08:57:28 -0500

    add package.swift back in

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "ZippyJSON",
+    platforms: [
+        .iOS(.v11.0),
+    ],
+    products: [
+        .library(
+            name: "ZippyJSON",
+            targets: ["ZippyJSON"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/michaeleisel/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.2")),
+        .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .upToNextMajor(from: "1.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "ZippyJSON",
+            dependencies: ["ZippyJSONCFamily", "JJLISO8601DateFormatter"]),
+        .testTarget(
+            name: "ZippyJSONTests",
+            dependencies: ["ZippyJSON"]),
+    ]
+)
