d1d6f5af25aea0cdd1b93ed6d52185cce0d28dfc
Michael Eisel <michael@weav.io>
2019-11-10 11:19:21 -0500

    commit

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/michaeleisel/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.2")),
-        .package(url: "../ZippyJSONCFamily", .branch("me-swift-pm")),
+        .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .branch("me-swift-pm")),
         // .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
