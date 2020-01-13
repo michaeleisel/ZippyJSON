72ff3224ca558baa64d3dc247909e4a7ba5848f0
Michael Eisel <michael.eisel@gmail.com>
2020-01-13 10:54:41 -0500

    fix JSONKey visibility for unit tests

--- Sources/ZippyJSON/ZippyJSONDecoder.swift
+++ Sources/ZippyJSON/ZippyJSONDecoder.swift
@@ -630,31 +630,31 @@ extension __JSONDecoder {
     }
 }
 
-fileprivate struct JSONKey : CodingKey {
+struct JSONKey : CodingKey {
     public var stringValue: String
     public var intValue: Int?
 
-    public init?(stringValue: String) {
+    init?(stringValue: String) {
         self.stringValue = stringValue
         self.intValue = nil
     }
 
-    public init?(intValue: Int) {
+    init?(intValue: Int) {
         self.stringValue = "\(intValue)"
         self.intValue = intValue
     }
 
-    public init(stringValue: String, intValue: Int?) {
+    init(stringValue: String, intValue: Int?) {
         self.stringValue = stringValue
         self.intValue = intValue
     }
 
-    fileprivate init(index: Int) {
+    init(index: Int) {
         self.stringValue = "Index \(index)"
         self.intValue = index
     }
 
-    fileprivate static let `super` = JSONKey(stringValue: "super")!
+    static let `super` = JSONKey(stringValue: "super")!
 }
 
 private final class JSONUnkeyedDecoder : UnkeyedDecodingContainer {
