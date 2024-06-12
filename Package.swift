25a7486e33d24f7e4d52419395e65f30767dabb6
Steven Hepting <steven.hepting@airbnb.com>
2024-06-12 14:46:06 -0700

    [Deps] Bump JJLISO8601DateFormatter version 1.50 → 1.7.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["ZippyJSON"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/michaeleisel/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.5")),
+        .package(url: "https://github.com/michaeleisel/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.7")),
         .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .exact("1.2.9")),
     ],
     targets: [
