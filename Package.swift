1dd1168b01e83711b8d2be00d2a0dec1cd84b492
Michael Eisel <michael.eisel@gmail.com>
2024-06-12 21:33:02 -0400

    bump to 1.2.14

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/michaeleisel/JJLISO8601DateFormatter", .upToNextMajor(from: "0.1.7")),
-        .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .exact("1.2.9")),
+        .package(url: "https://github.com/michaeleisel/ZippyJSONCFamily", .exact("1.2.14")),
     ],
     targets: [
         .target(
